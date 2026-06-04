e30767a907717b4b79fba34c1d950b219db9cb67
adzandaffakarim <adzandaffa1416Gmail.com>
2026-06-04 19:42:49 +0700

    perbaikan fungsi kategori

--- smart_garden_system/file_io.py
+++ smart_garden_system/file_io.py
@@ -2,16 +2,16 @@ def simpan_data(data):
     #Membuka file tanaman.txt dalam mode write (menimpa isi file lama)
     with open("data_tanaman.txt", "w") as f:
         for t in data:
-            f.write(f"{t.nama},{t.umur},{t.tinggi}\n") #Menyimpan data dengan format nama,umur,tinggi
+            f.write(f"{t.nama},{t.umur},{t.tinggi},{t.kategori}\n") #Menyimpan data dengan format nama,umur,tinggi
 
 def load_data():
     data = []
     try:
         #Membuka file dalam mode read
-        with open("data/tanaman.txt", "r") as f:
+        with open("data_tanaman.txt", "r") as f:
             for line in f:
-                nama, umur, tinggi = line.strip().split(",") #Menghapus enter dan pisahkan berdasarkan koma
-                data.append((nama, int(umur), int(tinggi))) #Simpan ke list sebagai tuple 
+                nama, umur, tinggi, kategori = line.strip().split(",") #Menghapus enter dan pisahkan berdasarkan koma
+                data.append((nama, int(umur), int(tinggi), str(kategori))) #Simpan ke list sebagai tuple 
     except:
         print("File belum ada, data kosong.")
 
