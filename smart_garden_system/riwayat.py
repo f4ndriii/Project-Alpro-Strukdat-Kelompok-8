f8f27ebdcdc0e66511cfe2454914a79de1196912
f4ndriii <alfandri10062007@gmail.com>
2026-06-10 18:23:25 +0700

    Mengganti struktur data untuk fungsi lihat riwayat dengan double linked list

--- smart_garden_system/riwayat.py
+++ smart_garden_system/riwayat.py
@@ -1,6 +1,6 @@
-from struktur_data import LinkedList
+from struktur_data import DoubleLinkedList
 
-riwayat = LinkedList()  # Linked List digunakan untuk menyimpan riwayat aktivitas pengguna
+riwayat = DoubleLinkedList()
 
 # Menambahkan aktivitas baru ke riwayat
 def tambah_riwayat(aksi):
@@ -8,5 +8,20 @@ def tambah_riwayat(aksi):
 
 # Menampilkan seluruh riwayat aktivitas
 def lihat_riwayat():
+    if riwayat.head is None:
+        print("=== Riwayat Aktivitas ===")
+        print("Belum ada riwayat aktivitas.")
+        return
+
     print("=== Riwayat Aktivitas ===")
-    riwayat.tampil()
+    pilihan = input("\nTampilkan dari:"
+                    "\n(1) Lama→Baru"
+                    "\natau (2) Baru→Lama"
+                    "\npilih: ")
+
+    if pilihan == "1":
+        riwayat.tampil_maju()
+    elif pilihan == "2":
+        riwayat.tampil_mundur()
+    else:
+        print("Pilihan tidak valid.")
